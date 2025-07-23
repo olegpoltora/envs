e0115fe5315699be20a08c139e9411caeba70f3f
Oleg Poltoratskii <opoltoratskii@gainsystems.com>
2025-07-24 02:39:58 +0400

    Sync with poltora env & rollback

--- run/install-env-common-settings.sh
+++ run/install-env-common-settings.sh
@@ -1,162 +1,74 @@
-read -p "Продолжить выполнение ${BASH_SOURCE[0]}? (y/n): " answer
-
-if ! [[ "$answer" =~ ^[YyДд] ]]; then
-    echo "Вы выбрали НЕТ. Выход..."
-    return
-fi
-echo "Вы выбрали ДА. Выполняем действие..."
-
 # Common environment
 
-#sudo add-apt-repository universe
-sudo apt-get update
+#DIR="$(cd "$(dirname "$0")" && pwd)"
+DIRS="$(dirname "$(readlink -f "$0")")"
+RUT="$(echo $DIRS | sed 's/\(\/backup\).*/\1/g')"
 
-## ~~encfs (notification)~~
+#echo $DIR
+#echo $DIRS
+#echo $RUT
 
-#sudo apt-get install -y encfs 
+shopt -s nullglob # Sets nullglob
+shopt -s nocaseglob # Sets nocaseglob
 
-## gocryptfs
-
-sudo apt install -y gocryptfs
-
-## file navigator
-
-sudo apt-get install -y mc  
-
-## backup
-
-sudo apt-get install -y backintime-qt4 meld 
-
-### ==check media==
-
-#sudo fsck -f /dev/sda1
-
-## soft manipulate
-
-sudo apt-get install -y synaptic 
-
-#sudo apt-get install -y hardinfo
-
-#sudo apt-get install -y exfat-fuse exfat-utils 
-
-## disk manipulate
-
-sudo apt-get install -y gparted
-
-## Startup disk creator
-
-sudo apt-get install -y usb-creator-gtk
-
-## Folder info
-
-sudo apt-get install -y baobab
-
-## Typora
-
-#sudo dpkg -i ./typora_1.2.4_amd64.deb
-#sudo dpkg -i /media/poltora/Terra/backintime/soft/poltora/1/last_snapshot/backup/mnt/poltora/Soft/install/typora_1.2.4_amd64.deb
-
-#typora + docker installation (apt-key is deprecated)
-#wget https://typora.io/linux/public-key.asc | sudo gpg --dearmor > /usr/share/keyrings/typora.gpg
-#echo "deb [arch=amd64 signed-by=/usr/share/keyrings/typora.gpg] https://typora.io/linux ./" | sudo tee /etc/apt/sources.list.d/typora.list > /dev/null
-
-#wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
-#sudo add-apt-repository 'deb https://typora.io/linux ./'
-
-#sudo apt-get update
-#sudo apt-get install typora -y
-
-sudo snap install typora
-
-## Office
-
-#sudo apt-get install libreoffice-impress -y
-
-sudo apt install -y gedit
-
-## Pdf search
-
-sudo apt install -y pdfgrep
-
-## ~~freemind~~
-
-#freemind через снеп - гавно. Запускаю .sh
-
-#sudo apt-get install -y freemind
-
-## ~~aws~~
-
-#sudo apt-get install -y s3cmd
-
-## image-magic for convert
-
-sudo apt install -y graphicsmagick-imagemagick-compat
-
-## ~~curlftpfs (монтирование ftp)~~
-
-#sudo apt install -y curlftpfs
-
-#mkdir -p ~/ftp_mount
-
-## ~~grsync~~
-
-#sudo apt install -y grsync
-
-## ~~Self performance~~
-
-#pomodorro
-
-#sudo snap install pomatez
-
-#sudo apt install -y xfce4-time-out-plugin
-
-## ~~Imagemagick~~
-
-#sudo apt install imagemagick -y 
-
-# Configuration
-
-#read -p "Now setting configuration…(Crtl-C or ENTER)"
+CURRENTDATE=`date +"%Y-%m-%d %T"`
 
 ## Common utils
-
-#read -p "Common utils…(Crtl-C or ENTER)"
 
 echo "export PATH=\$PATH:/mnt/poltora/utils/" >> ~/.bashrc
 echo "export PATH=\$PATH:/mnt/poltora/utils/" >> ~/.profile
 
-## ~~Zoom desktop~~
+# Settings
 
-#xfconf-query --set false --channel xfwm4 --property /general/zoom_desktop
+read -p "Used backup file path: $RUT... (Crtl-C or ENTER)"
 
-## mnt
+## back in time
 
-sudo mkdir /mnt/poltora/
-sudo chown poltora:poltora /mnt/poltora/
-mkdir /mnt/poltora/test
-echo "ls -la /mnt/poltora"
-ls -la /mnt/poltora
-read -p "…"
+mkdir -p ~/.config/backintime/
+cp -rf ~/.config/backintime ~/.config/backintime.backup-"${CURRENTDATE}"
+cp -rf $RUT/../config ~/.config/backintime/config
 
-## backup (waiting for devices for all pc…)
+## xubuntu
 
-#sudo mkdir /mnt/backup/
-#sudo chown poltora:poltora /mnt/backup/
-#mkdir /mnt/backup/test
-#echo "ls -la /mnt/backup"
-#ls -la /mnt/backup
-#read -p "…"
+mkdir -p ~/.config/xfce4/
+cp -rf ~/.config/xfce4 ~/.config/xfce4.backup-"${CURRENTDATE}"
+cp -rf $RUT/home/poltora/.config/xfce4/* ~/.config/xfce4/
 
-## priv
+## ftp client (gigolo)
 
-mkdir /mnt/poltora/.priv
-mkdir /mnt/poltora/.priv.sec
-gocryptfs -init /mnt/poltora/.priv.sec
-gocryptfs /mnt/poltora/.priv.sec /mnt/poltora/.priv
-#encfs /mnt/poltora/.priv.sec /mnt/poltora/.priv
+mkdir -p ~/.config/gigolo/
+cp -rf ~/.config/gigolo ~/.config/gigolo.backup-"${CURRENTDATE}"
+cp -rf $RUT/home/poltora/.config/gigolo/bookmarks ~/.config/gigolo/
 
-echo "ls -la /mnt/poltora/.priv"
-ls -la /mnt/poltora/.priv
-read -p "..."
+## midnight commander
+
+mkdir -p ~/.config/mc/
+cp -rf ~/.config/mc ~/.config/mc.backup-"${CURRENTDATE}"
+cp -rf $RUT/home/poltora/.config/mc/hotlist ~/.config/mc/
+
+echo export HISTCONTROL=ignoreboth:erasedups >> ~/.bashrc
+
+## typora
+
+mkdir -p ~/.config/Typora/conf
+cp -rf ~/.config/Typora/conf ~/.config/Typora/conf.backup-"${CURRENTDATE}"
+cp -rf $RUT/home/poltora/.config/Typora/conf ~/.config/Typora/
+cp -rf ~/.config/Typora/profile.data ~/.config/Typora/profile.data.backup-"${CURRENTDATE}"
+cp -rf $RUT/home/poltora/.config/Typora/profile.data ~/.config/Typora/
+
+## libre office
+
+mkdir -p ~/.config/libreoffice/4/user
+cp -rf ~/.config/libreoffice/4/user ~/.config/libreoffice/4/user.backup-"${CURRENTDATE}"
+cp -rf $RUT/home/poltora/.config/libreoffice/4/user/registrymodifications.xcu ~/.config/libreoffice/4/user/registrymodifications.xcu 
+
+## convert jpg to pdf
+
+sudo sed -i  's|<policy domain="coder" rights="none" pattern="PDF" />|<!-- <policy domain="coder" rights="none" pattern="PDF"/> -->|' /etc/ImageMagick-6/policy.xml
+
+
+shopt -u nocaseglob # Unsets nocaseglob
+shopt -u nullglob # Unsets nullglob
+
 
 
--- run/install-env-common.sh
+++ run/install-env-common.sh
@@ -11,6 +11,14 @@ echo "Вы выбрали ДА. Выполняем действие..."
 #sudo add-apt-repository universe
 sudo apt-get update
 
+## ~~encfs (notification)~~
+
+#sudo apt-get install -y encfs
+
+## gocryptfs
+
+sudo apt install -y gocryptfs
+
 ## file navigator
 
 sudo apt-get install -y mc  
@@ -43,27 +51,52 @@ sudo apt-get install -y usb-creator-gtk
 
 sudo apt-get install -y baobab
 
-## Typora (markdown editor)
+## Typora
 
 #sudo dpkg -i ./typora_1.2.4_amd64.deb
 #sudo dpkg -i /media/poltora/Terra/backintime/soft/poltora/1/last_snapshot/backup/mnt/poltora/Soft/install/typora_1.2.4_amd64.deb
 
-wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
-sudo add-apt-repository -y 'deb https://typora.io/linux ./'
-sudo apt-get update
-sudo apt-get install typora -y
+#typora + docker installation (apt-key is deprecated)
+#wget https://typora.io/linux/public-key.asc | sudo gpg --dearmor > /usr/share/keyrings/typora.gpg
+#echo "deb [arch=amd64 signed-by=/usr/share/keyrings/typora.gpg] https://typora.io/linux ./" | sudo tee /etc/apt/sources.list.d/typora.list > /dev/null
+
+#wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+#sudo add-apt-repository 'deb https://typora.io/linux ./'
+
+#sudo apt-get update
+#sudo apt-get install typora -y
+
+sudo snap install typora
 
 ## Office
 
 #sudo apt-get install libreoffice-impress -y
 
-#sudo apt install -y gedit
+sudo apt install -y gedit
+
+## Pdf search
+
+sudo apt install -y pdfgrep
 
 ## ~~freemind~~
 
 #freemind через снеп - гавно. Запускаю .sh
 
 #sudo apt-get install -y freemind
+
+## ~~aws~~
+
+#sudo apt-get install -y s3cmd
+
+## image-magic for convert
+
+sudo apt install -y graphicsmagick-imagemagick-compat
+
+## ~~curlftpfs (монтирование ftp)~~
+
+#sudo apt install -y curlftpfs
+
+#mkdir -p ~/ftp_mount
 
 ## ~~grsync~~
 
@@ -77,9 +110,9 @@ sudo apt-get install typora -y
 
 #sudo apt install -y xfce4-time-out-plugin
 
-## Imagemagick
+## ~~Imagemagick~~
 
-sudo apt install imagemagick -y 
+#sudo apt install imagemagick -y
 
 # Configuration
 
@@ -95,4 +128,35 @@ echo "export PATH=\$PATH:/mnt/poltora/utils/" >> ~/.profile
 ## ~~Zoom desktop~~
 
 #xfconf-query --set false --channel xfwm4 --property /general/zoom_desktop
+
+## mnt
+
+sudo mkdir /mnt/poltora/
+sudo chown poltora:poltora /mnt/poltora/
+mkdir /mnt/poltora/test
+echo "ls -la /mnt/poltora"
+ls -la /mnt/poltora
+read -p "…"
+
+## backup (waiting for devices for all pc…)
+
+#sudo mkdir /mnt/backup/
+#sudo chown poltora:poltora /mnt/backup/
+#mkdir /mnt/backup/test
+#echo "ls -la /mnt/backup"
+#ls -la /mnt/backup
+#read -p "…"
+
+## priv
+
+mkdir /mnt/poltora/.priv
+mkdir /mnt/poltora/.priv.sec
+gocryptfs -init /mnt/poltora/.priv.sec
+gocryptfs /mnt/poltora/.priv.sec /mnt/poltora/.priv
+#encfs /mnt/poltora/.priv.sec /mnt/poltora/.priv
+
+echo "ls -la /mnt/poltora/.priv"
+ls -la /mnt/poltora/.priv
+read -p "..."
+
 
