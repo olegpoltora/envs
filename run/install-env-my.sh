fd957d61836195e48cab363d4ae1e5c69a040f66
Oleg Poltoratskii <oleg.poltoratskii@gmail.com>
2025-07-24 13:54:03 +0400

    Update install-env-my.sh

--- run/install-env-my.sh
+++ run/install-env-my.sh
@@ -1,24 +1,51 @@
 # My environment
 
-sudo add-apt-repository universe
+#sudo add-apt-repository universe
 sudo apt-get update
 
-## Common soft
-### encfs (asks license)
-sudo apt-get install -y encfs 
+## ~~JRE~~
+
+#sudo apt install -y default-jre
+
+#java -version
+
+## ~~Lightread~~
+
+#sudo add-apt-repository ppa:cooperjona/lightread
+
+#sudo apt-get update && sudo apt-get install lightread
 
 ## ~~aws~~
 
 #sudo apt-get install -y s3cmd
 
+
 # Settings
 
+read -p "Now setting configuration…(Crtl-C or ENTER)"
 
-## encfs
+## backup
 
-mkdir /mnt/poltora/.priv
-mkdir /mnt/poltora/.priv.sec
-encfs /mnt/poltora/.priv.sec /mnt/poltora/.priv
+sudo mkdir /mnt/backup/
+sudo chown poltora:poltora /mnt/backup/
+mkdir /mnt/backup/test
+echo "ls -la /mnt/backup"
+ls -la /mnt/backup
+read -p "…"
+
+## backup-priv
+
+mkdir /mnt/backup/.priv
+mkdir /mnt/backup/.priv-go.sec
+gocryptfs /mnt/backup/.priv-go.sec /backup/.priv
+
+echo "ls -la /mnt/backup/.priv"
+ls -la /mnt/backup/.priv
+read -p "..."
+
+
+
+
 
 
 
