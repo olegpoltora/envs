af1e036d1b1ab6545a3d7278d309fc0a8050bc35
Oleg Poltoratskii <oleg.poltoratskii@gmail.com>
2025-07-24 14:51:03 +0400

    update media

--- run/install-env-media.sh
+++ run/install-env-media.sh
@@ -15,7 +15,7 @@ sudo apt-get update
 
 ## System
 
-sudo apt-get install -y xubuntu-restricted-extras 
+#sudo apt-get install -y xubuntu-restricted-extras 
 
 #exfat-fuse exfat-utils
 
@@ -23,11 +23,26 @@ sudo apt-get install -y xubuntu-restricted-extras
 
 sudo apt-get install -y vsftpd
 
+sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.back-$(date +"%Y-%m-%d-%H-%M-%S")
+
+sudo sed -i  's|#write_enable=YES|write_enable=YES|' /etc/vsftpd.conf
+
+echo 'mdtm_write=YES
+' | sudo tee -a /etc/vsftpd.conf
+
+cat /etc/vsftpd.conf | grep mdtm
+
+sudo service vsftpd restart
+
+sudo service vsftpd status
+
+#how to restore original conf file? remove with --purge and install again
+
 ## Media soft
 
 ### Torrent
 
-sudo apt install -y qbittorrent
+#sudo apt install -y qbittorrent
 #sudo apt-get install -y qbittorrent
 
 #sudo apt-get install -y deluge
@@ -37,3 +52,20 @@ sudo apt install -y qbittorrent
 #sudo snap install vlc
 
 sudo apt install -y vlc
+
+### acestreamplayer
+
+#sudo apt install -y acestreamplayer
+
+sudo snap install acestreamplayer
+
+# Media Environment settings
+
+## transmission
+
+#sed -i  's|"preallocation": 1|"preallocation": 2|' /home/poltora/.config/transmission/settings.json
+
+sed -i  's|"queue-stalled-minutes": 30|"queue-stalled-minutes": 3|' /home/poltora/.config/transmission/settings.json
+
+
+
