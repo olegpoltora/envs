d5d87af0ebb4a76e8d63883da818546e761ed909
Oleg Poltoratskii <oleg.poltoratskii@gmail.com>
2025-07-26 04:10:52 +0400

    Add files via upload

--- run/install-env-work.sh
+++ run/install-env-work.sh
@@ -3,6 +3,18 @@
 #sudo add-apt-repository universe
 #sudo apt-get update
 
+## rename
+
+sudo apt install -y rename
+
+### ~~Command-line JSON processor (gitlab api)~~
+
+#sudo apt-get install -y jq
+
+## postman
+
+sudo snap install postman
+
 ## curl
 
 sudo apt install -y curl
@@ -26,21 +38,21 @@ if [ -f /etc/vsftpd.conf ]; then
   sudo service vsftpd restart
 fi
 
-sudo service vsftpd status
+#sudo service vsftpd status
 
 #how to restore original conf file? remove with --purge and install again
 
-## VPN
+## ~~VPN~~
 
-sudo apt install -y openconnect
+#sudo apt install -y openconnect
 
-### globalprotect-openconnect ==!==
+### ~~globalprotect-openconnect~~
 
 #http://system-administrator.pages.cs.sun.ac.za/globalprotect-openconnect/#globalprotect-openconnect-ubuntu-2004-and-2204
 
-sudo add-apt-repository -y ppa:yuezk/globalprotect-openconnect
-sudo apt-get update
-sudo apt install -y globalprotect-openconnect
+#sudo add-apt-repository -y ppa:yuezk/globalprotect-openconnect
+#sudo apt-get update
+#sudo apt install -y globalprotect-openconnect
 
 ## ~~RDP~~
 
@@ -59,3 +71,24 @@ sudo apt install -y globalprotect-openconnect
 # shellcheck disable=SC2016
 grep -qxF 'export PATH=$PATH:/mnt/poltora/Documents/utils/' ~/.bashrc || echo 'export PATH=$PATH:/mnt/poltora/Documents/utils/' >> ~/.bashrc
 grep -qxF 'export PATH=$PATH:/mnt/poltora/Documents/utils/' ~/.profile || echo 'export PATH=$PATH:/mnt/poltora/Documents/utils/' >> ~/.profile
+
+## backup
+
+sudo mkdir -p /media/poltora/work-backup/
+sudo chown poltora:poltora /media/poltora/work-backup/
+echo "ls -la /media/poltora/work-backup/"
+ls -la /media/poltora/work-backup/
+#read -p "…"
+
+
+## backup-priv
+
+mkdir -p /media/poltora/work-backup/.priv
+mkdir -p /media/poltora/work-backup/.priv.sec
+gocryptfs -init /media/poltora/work-backup/.priv.sec
+gocryptfs /media/poltora/work-backup/.priv.sec /media/poltora/work-backup/.priv
+
+echo "ls -la /media/poltora/work-backup/.priv"
+ls -la /media/poltora/work-backup/.priv
+#read -p "..."
+
