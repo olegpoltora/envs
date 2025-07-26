12c5023e5706ba4d36d44ff638792e2325d1f99c
Oleg Poltoratskii <oleg.poltoratskii@gmail.com>
2025-07-26 16:27:13 +0400

    Add files via upload

--- utils/system-info.sh
+++ utils/system-info.sh
@@ -1,1 +1,31 @@
+#sudo dmidecode | less
+
+#echo "==============================System Information=============================="
+#uname -v
+#uname -n
+uname -a
 lsb_release -a
+#echo "System serial number"
+#sudo dmidecode -s system-serial-number
+#echo "bios-vendor"
+#sudo dmidecode -s bios-vendor
+#echo "bios-version"
+#sudo dmidecode -s bios-version
+
+#sudo dmidecode |grep -E "system-serial-number|bios-vendor|bios-version"
+#sudo dmidecode |grep system-serial-number
+
+#read -p "==============================CPU=============================="
+#lscpu
+
+#read -p "==============================Hardware=============================="
+#lshw
+
+#read -p "==============================Block Device Information=============================="
+#lsblk
+
+#read -p "==============================USB Controller=============================="
+#lsusb
+
+#read -p "==============================PCI Devices=============================="
+#lspci -v
